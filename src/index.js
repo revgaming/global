d8c5aded20c1a8e1578de8cde319f7a593bf817a
estagent <estagent@chip.me>
2020-12-28 08:32:50 +0300

    languages dependency upgraded to 2.x

--- src/index.js
+++ src/index.js
@@ -1,36 +1,18 @@
 import {loader, loadStringPlugins} from '@revgaming/helpers'
-import {globalizeConfig, mergeConfigs} from '@revgaming/config'
-import languages from '@revgaming/languages'
-import currencies from '@revgaming/currencies'
-import location from '@revgaming/location'
-import appearance from '@revgaming/appearance'
-import media from '@revgaming/media'
-import init from './mixins/init'
+import {bootLanguages} from '@revgaming/languages'
+import {bootConfig} from '@revgaming/config'
+import {bootCurrencies} from '@revgaming/currencies'
+import {bootLocation} from '@revgaming/location'
+import {bootAppearance} from '@revgaming/appearance'
+import {isMuted} from '@revgaming/media'
 
 export default function (opts = {}) {
   loadStringPlugins()
-  if (opts.hasOwnProperty('configs')) mergeConfigs(opts.configs)
-  globalizeConfig('config')
+  bootConfig(opts)
   return loader({})
-    .load(mergeConfigs)
-    .load(
-      languages({
-        locale: opts.hasOwnProperty('locale') ? opts.locale : 'detect', // to by-pass detection
-        default_priority: opts.default_priority === true, // if app.locale (default) seen in browser lang list, dont try to set others.
-        fallback_priority: opts.fallback_priority === true,
-        unsupported: opts.hasOwnProperty('unsupported_locale')
-          ? opts.unsupported
-          : null,
-        global: opts.hasOwnProperty('lang_global') ? opts.lang_global : '__', //  to globalize lang  lang//__
-        translations: typeof opts.hasOwnProperty('translations')
-          ? opts.translations
-          : {},
-      }),
-    )
-    .load(appearance())
-    .load(location())
-    .load(currencies())
-    .load(media())
-    .load(init)
-    .init()
+    .load(bootLanguages(opts))
+    .load(bootLocation(opts))
+    .load(bootCurrencies(opts))
+    .load(bootAppearance(opts))
+    .load(isMuted)
 }
