1f7142ea34947831a31fa06500959d9315e1a492
estagent <estagent@chip.me>
2020-12-28 14:04:50 +0300

    ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,11 @@
 const webpack = require('webpack')
 const path = require('path')
 
-const TerserPlugin = require("terser-webpack-plugin");
-
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
 
 let isProduction = false
 
 require('dotenv').config()
-
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  optimization: {
-    minimize: isProduction,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          ecma: 8,
-          safari10: true
-        }
-      })
-    ]
-  },
-  watchOptions: {
-    aggregateTimeout: 200,
-    poll: 500,
-    ignored: 'node_modules/**',
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin([
-      'APP_NAME',
-      'APP_VERSION',
-      'APP_URL',
-      'APP_ENV',
-      'APP_SSL',
-      'APP_DEBUG',
-      'APP_TIMEZONE',
-      'APP_LOCALE',
-      'APP_CURRENCY',
-      'NODE_ENV',
-    ]),
-    new LodashModuleReplacementPlugin,
-  ],
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve('./src'),
-    },
-  },
-}
 
 module.exports = (env, argv) => {
   isProduction = env.production === true
@@ -74,5 +18,56 @@ module.exports = (env, argv) => {
   console.log('APP_ENV: ', process.env.APP_ENV)
   console.log('isProduction: ', isProduction) // bool
 
-  return config
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'app.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: 8,
+            safari10: true,
+          },
+        }),
+      ],
+    },
+    watchOptions: {
+      aggregateTimeout: 200,
+      poll: 500,
+      ignored: 'node_modules/**',
+    },
+    plugins: [
+      new webpack.EnvironmentPlugin([
+        'APP_NAME',
+        'APP_VERSION',
+        'APP_URL',
+        'APP_ENV',
+        'APP_SSL',
+        'APP_DEBUG',
+        'APP_TIMEZONE',
+        'APP_LOCALE',
+        'APP_CURRENCY',
+        'NODE_ENV',
+      ])
+    ],
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  }
 }
