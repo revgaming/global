7d4dfce407ff5f50e3619cb4de5483517c87ec7a
estagent <estagent@chip.me>
2020-12-31 02:42:56 +0300

    config updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {loader, loadStringPlugins} from '@revgaming/helpers'
 import {bootLanguages} from '@revgaming/languages'
-import {bootConfig, mergeConfigs} from '@revgaming/config'
+import {bootConfig, mergeConfig} from '@revgaming/config'
 import {bootCurrencies} from '@revgaming/currencies'
 import {bootLocation} from '@revgaming/location'
 import {bootAppearance} from '@revgaming/appearance'
@@ -10,7 +10,7 @@ export default function (opts = {}) {
   loadStringPlugins()
   bootConfig(opts.config)
   return loader()
-    .load(mergeConfigs)
+    .load(mergeConfig)
     .load(bootLanguages(opts.languages))
     .load(bootLocation(opts.timezone))
     .load(bootCurrencies(opts.currencies))
