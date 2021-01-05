5790fe5732ed2e19bf5198fa75afd64eddb247ad
estagent <estagent@chip.me>
2021-01-05 20:33:25 +0300

    identifications => identity

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import {bootLanguages} from '@revgaming/languages'
 import {bootLocation} from '@revgaming/location'
 import {bootCurrencies} from '@revgaming/currencies'
 import {bootAppearance} from '@revgaming/appearance'
-import {bootDevice} from '@revgaming/device'
 import {bootMedia} from '@revgaming/media'
 import {bootSession} from '@revgaming/session'
-import {bootIdentifications} from '@revgaming/identifications'
+import {bootIdentity} from '@revgaming/identity'
 export default function (opts = {}) {
   require('./plugins')
   return loader()
@@ -16,8 +15,7 @@ export default function (opts = {}) {
     .load(bootLocation(opts.location))
     .load(bootCurrencies(opts.currencies))
     .load(bootAppearance(opts.appearance))
-    .load(bootDevice(opts.device))
-    .load(bootIdentifications(opts.identifications))
     .load(bootMedia(opts.media))
+    .load(bootIdentity(opts.identity))
     .load(bootSession(opts.session))
 }
